d785ad57ae98c77ea545bc47c841b12729d09156
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-18 17:30:12 -0500

    Connection to MongoDB & First commit

---
+++ src/main/java/ec/edu/espe/pruebaparcial2galarza/PruebaParcial2GalarzaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaparcial2galarza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaParcial2GalarzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaParcial2GalarzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/pruebaparcial2galarza/PruebaParcial2GalarzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaparcial2galarza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaParcial2GalarzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
