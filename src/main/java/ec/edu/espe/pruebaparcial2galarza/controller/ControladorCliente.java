267c2f7f7cab2ace31e09ae6b27c620143331775
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-18 21:11:25 -0500

    Add exceptions

--- src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorCliente.java
+++ src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorCliente.java
@@ -2,6 +2,7 @@ package ec.edu.espe.pruebaparcial2galarza.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -22,10 +23,13 @@ public class ControladorCliente {
 
 	@GetMapping("/{cedula}")
 	public ResponseEntity<ClienteRS> obtenerInformacionCliente(String cedula) {
-		Cliente cliente = servicioCliente.obtenerInformacionCliente(cedula);
-		ClienteRS clienteRS = ClienteMapper.mapToClienteRS(cliente);
-
-		return ResponseEntity.ok(clienteRS);
+		try {
+			Cliente cliente = servicioCliente.obtenerInformacionCliente(cedula);
+			ClienteRS clienteRS = ClienteMapper.mapToClienteRS(cliente);
+			return ResponseEntity.ok(clienteRS);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 }
--- src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorTurnos.java
+++ src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorTurnos.java
@@ -1,5 +1,6 @@
 package ec.edu.espe.pruebaparcial2galarza.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,29 +27,44 @@ public class ControladorTurnos {
 	@PostMapping("/generar-turno")
 	public ResponseEntity<Turno> generarTurno(@RequestBody String cedulaCliente,
 			String nombreCliente) {
-		Turno turno = servicioTurno.generarTurno(cedulaCliente, nombreCliente);
-		return ResponseEntity.ok(turno);
+		try {
+			Turno turno = servicioTurno.generarTurno(cedulaCliente, nombreCliente);
+			return ResponseEntity.ok(turno);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@PostMapping("/registrar-inicio-atencion")
 	public ResponseEntity<Turno> registrarInicioDeAtencion(@RequestBody String numeroTurno,
 			String codigoDeUsuario) {
-		Turno turno = servicioTurno.registrarInicioAtencion(numeroTurno, codigoDeUsuario);
-		return ResponseEntity.ok(turno);
+		try {
+			Turno turno = servicioTurno.registrarInicioAtencion(numeroTurno, codigoDeUsuario);
+			return ResponseEntity.ok(turno);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@PutMapping("/registrar-fin-atencion")
 	public ResponseEntity<Turno> registrarFinDeAtencion(@RequestBody String numeroTurno) {
-		Turno turno = servicioTurno.registroFinAtencion(numeroTurno);
-		return ResponseEntity.ok(turno);
+		try {
+			Turno turno = servicioTurno.registroFinAtencion(numeroTurno);
+			return ResponseEntity.ok(turno);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping("/calificar-atencion/{cedulaCliente}/{numeroTurno}/{calificacion}")
 	public ResponseEntity<Turno> calificarAtencion(@PathVariable("cedulaCliente") String cedulaCliente,
 			@PathVariable("numeroTurno") String numeroTurno,
 			@PathVariable("calificacion") Integer calificacion) {
-		Turno turno = servicioTurno.calificarTurno(cedulaCliente, numeroTurno, calificacion);
-		return ResponseEntity.ok(turno);
+		try {
+			Turno turno = servicioTurno.calificarTurno(cedulaCliente, numeroTurno, calificacion);
+			return ResponseEntity.ok(turno);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
-
 }
