cd3fcb53684dee69642251b4e57dfdd51674933f
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-18 20:54:09 -0500

    Fix Controladores

--- src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorTurnos.java
+++ src/main/java/ec/edu/espe/pruebaparcial2galarza/controller/ControladorTurnos.java
@@ -8,7 +8,9 @@ import ec.edu.espe.pruebaparcial2galarza.model.Turno;
 import ec.edu.espe.pruebaparcial2galarza.service.ServicioTurno;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -21,29 +23,30 @@ public class ControladorTurnos {
 		this.servicioTurno = servicioTurno;
 	}
 
-	@GetMapping("/generar-turno")
+	@PostMapping("/generar-turno")
 	public ResponseEntity<Turno> generarTurno(@RequestBody String cedulaCliente,
 			String nombreCliente) {
 		Turno turno = servicioTurno.generarTurno(cedulaCliente, nombreCliente);
 		return ResponseEntity.ok(turno);
 	}
 
-	@PostMapping("/registrar-inicio-atencion")
+	@PutMapping("/registrar-inicio-atencion")
 	public ResponseEntity<Turno> registrarInicioDeAtencion(@RequestBody String numeroTurno,
 			String codigoDeUsuario) {
 		Turno turno = servicioTurno.registrarInicioAtencion(numeroTurno, codigoDeUsuario);
 		return ResponseEntity.ok(turno);
 	}
 
-	@PostMapping("/registrar-fin-atencion")
-	public ResponseEntity<Turno> registrarFinDeAtencion(@RequestBody String numeroTurno) {
+	@GetMapping("/registrar-fin-atencion/{numeroTurno}")
+	public ResponseEntity<Turno> registrarFinDeAtencion(@PathVariable("numeroTurno") String numeroTurno) {
 		Turno turno = servicioTurno.registroFinAtencion(numeroTurno);
 		return ResponseEntity.ok(turno);
 	}
 
-	@PostMapping("/calificar-atencion")
-	public ResponseEntity<Turno> calificarAtencion(@RequestBody String cedulaCliente, String numeroTurno,
-			Integer calificacion) {
+	@GetMapping("/calificar-atencion/{cedulaCliente}/{numeroTurno}/{calificacion}")
+	public ResponseEntity<Turno> calificarAtencion(@PathVariable("cedulaCliente") String cedulaCliente,
+			@PathVariable("numeroTurno") String numeroTurno,
+			@PathVariable("calificacion") Integer calificacion) {
 		Turno turno = servicioTurno.calificarTurno(cedulaCliente, numeroTurno, calificacion);
 		return ResponseEntity.ok(turno);
 	}
