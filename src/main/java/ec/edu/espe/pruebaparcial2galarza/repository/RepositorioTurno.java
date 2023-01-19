2d75fd9f9cfc77371bae30d95f603d459dbc85cc
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-18 19:37:09 -0500

    Add Repository

---
+++ src/main/java/ec/edu/espe/pruebaparcial2galarza/repository/RepositorioTurno.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.pruebaparcial2galarza.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ec.edu.espe.pruebaparcial2galarza.model.Turno;
+
+public interface RepositorioTurno extends CrudRepository<Turno, String> {
+
+}
